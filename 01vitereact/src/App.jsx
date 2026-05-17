5166eb668b33f379c8d16f580b0c5e091d170d41
Shivam <shivamgarade05@gmail.com>
2026-05-18 00:50:04 +0530

    basics , creating , cleaning of react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react | shivam </h1>
+      <p>Gaining confidence in react because of hitesh sir</p>
+    </>
+  )
+}
+
+export default App
