bffec246fa6fd08c9544ea8ee670297adb609554
Shivam <shivamgarade05@gmail.com>
2026-05-18 17:06:31 +0530

    creating own react and understanding under the hood

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./Chai.jsx"
 function App() {
+  const username = "shivam"
 
   return (
     <>
       <Chai />
+      <h1>Hello {username}</h1>
       <p>Gaining confidence in react because of hitesh sir</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,52 @@
 import { createRoot } from 'react-dom/client' // react provide karta hai ki aap apne khud ka dom create kar lo using createRoot function 
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(<App />)
+
+// function MyApp(){
+//     return (
+//         <div>
+//             <h1>Custom app</h1>
+//         </div>
+//     )
+// }
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    children: "Click me to visit google"
+}
+
+
+
+// const anotherElement = (
+//     <a href="https://www.google.com">Go to Google</a>
+// )
+
+
+const anotherUser = 'chai aur react'
+
+// const reactElement = React.createElement(
+//     'a',
+//     {
+//         href: 'https://www.google.com',
+//         target: '_blank',
+//     },
+//     "Click me to visit google",
+//     anotherUser
+// )
+
+
+createRoot(document.getElementById('root')).render(
+    // anotherElement
+    // reactElement
+    <App />
+)
 // just create root ki madat se dom create kiye id ke through root div ko select karke usmein app ko render kar diye
 // function ke andar html return kardo and main us html ko render karva dunga
 // in the end java script ke through html render karva rahe
