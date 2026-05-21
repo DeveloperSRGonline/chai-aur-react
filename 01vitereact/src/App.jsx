70589eb0188305794cac4045c1d1f48f9a452bcf
Shivam <shivamgarade05@gmail.com>
2026-05-21 17:07:46 +0530

    01

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <Chai />
+      {/* whatever in curlybraces will be treated as variable */}
+      {/* it is called as expression (evaluated expression) js ka final outcome jo evaluauted ho gaya hai vo likhe hai*/}
+      {/* kyo mai ismein if syntax or loop or like that nahi likh sakte */}
       <h1>Hello {username}</h1>
       <p>Gaining confidence in react because of hitesh sir</p>
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 
+// agar app ek function hai toh mai function ko parse kar ke dekh leta hu directly from here for testing just by making it in main.jsx 
 
-// function MyApp(){
-//     return (
-//         <div>
-//             <h1>Custom app</h1>
-//         </div>
-//     )
-// }
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom app !</h1>
+        </div>
+    )
+}
+
+//  ye jsx wale code ko parsing in like object formate ye kaam bhi bundler ka hi hota hai
 
 
 const reactElement = {
@@ -25,29 +28,40 @@ const reactElement = {
 
 
 
-// const anotherElement = (
-//     <a href="https://www.google.com">Go to Google</a>
-// )
+const anotherElement = (
+    <a href="https://www.google.com">Go to Google</a>
+)
 
 
 const anotherUser = 'chai aur react'
 
-// const reactElement = React.createElement(
-//     'a',
-//     {
-//         href: 'https://www.google.com',
-//         target: '_blank',
-//     },
-//     "Click me to visit google",
-//     anotherUser
-// )
+// as per react required formate 
+const realReactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    "Click me to visit google",
+    // anotherUser // to put any value using variable - agar another user mein if else ka code likhoge toh in the end vo object mein hi convert ho raha hai and object me toh expressions nahi likh sakte na
+)
 
 
 createRoot(document.getElementById('root')).render(
+
     // anotherElement
     // reactElement
-    <App />
+    realReactElement
+    // <MyApp /> - har react ek buldler use karta hai bable / vite (behind he scene magic)
+    // <MyApp /> --> MyApp() is ko aise bhi agar like toh kya karen at the end react bhi toh js hi hai 
+    // <App />
 )
 // just create root ki madat se dom create kiye id ke through root div ko select karke usmein app ko render kar diye
 // function ke andar html return kardo and main us html ko render karva dunga
 // in the end java script ke through html render karva rahe
+
+// ek curiosity ki agar vo function / method ko likhne ki jagah mai agar directly hi pass kardu in formate of that object to save the steps is it possible ? 
+//  <reactElement /> aisa likhane se toh "a" tag aa jana chahiye tha phir kyo nahi aaya 
+// asia bhi toh ho sakte hai ki function ko hi aspect kiya ja raha ho par hum toh object ko execute karne ki koshish kar rahe hai
+
+// ek reason hai ki jo hum ne object banaya uske liye custom render ka code bhi hum ne likha tha - par react mein toh jo render ka code hai vo toh react ne mera liye likha hai vo "render" ek method hai toh vo kuchh(kisi) type ke parameter except kar rahi hogi vo kaisa object expect kar rahe ispar sab depend karta hai
