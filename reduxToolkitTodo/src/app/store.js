015f8bfef0053155f9b4b70d2c89494422d32998
Shivam <shivamgarade05@gmail.com>
2026-06-02 23:44:31 +0530

    if multiple todos are there then do like this

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -1,4 +1,12 @@
 import { configureStore} from "@reduxjs/toolkit"
+// using the reducer to update changes here
+import todoReducer from "../features/todo/todoSlice"
 
 // to store global data
-export const store = configureStore({})
+export const store = configureStore({
+    reducer: {
+        todo: todoReducer,  // Ab todo ka sara data 'state.todo' mein milega
+        auth: authReducer,  // Auth ka data 'state.auth' mein milega
+        cart: cartReducer   // Cart ka data 'state.cart' mein milega
+    }
+})
