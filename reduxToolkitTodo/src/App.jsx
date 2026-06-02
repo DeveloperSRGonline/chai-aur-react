c67f73833503b3c0160962855d12773e3840f8dc
Shivam <shivamgarade05@gmail.com>
2026-06-03 00:31:54 +0530

    add todos component to app.jsx

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
 
 const App = () => {
   return (
     <div>
       <h1>ReduxToolkit Todo App</h1>
       <AddTodos />
+      <Todos />
     </div>
   )
 }
