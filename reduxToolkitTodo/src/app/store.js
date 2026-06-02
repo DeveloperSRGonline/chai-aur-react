13ac455f5f0e047d6960149bb62c20031a5e05ac
Shivam <shivamgarade05@gmail.com>
2026-06-02 23:45:27 +0530

    add : todReducer in store

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -4,9 +4,5 @@ import todoReducer from "../features/todo/todoSlice"
 
 // to store global data
 export const store = configureStore({
-    reducer: {
-        todo: todoReducer,  // Ab todo ka sara data 'state.todo' mein milega
-        auth: authReducer,  // Auth ka data 'state.auth' mein milega
-        cart: cartReducer   // Cart ka data 'state.cart' mein milega
-    }
+    reducer:todoReducer
 })
