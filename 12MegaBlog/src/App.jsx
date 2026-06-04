dbfae44323bc6a9d9004928107bd5c4a717a6407
Shivam <shivamgarade05@gmail.com>
2026-06-04 21:06:22 +0530

    add : app.jsx functionality get current user when component loads and add header and footer components

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth.service"
+import { login, logout } from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import "./App.css"
+
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
   return (
     <div>
-      <h1>A blog app with Appwrite</h1>
+      {loading ? <h1>Loading...</h1> :
+        <div className="">
+          <div>
+            <Header />
+            <main>
+              
+            </main>
+            <Footer />
+          </div>
+        </div>}
     </div>
   )
 }
