0c313474b6719c6df7230f9b7ea5f84a8aa350b9
Shivam <shivamgarade05@gmail.com>
2026-06-08 19:26:13 +0530

    add postCard component and its styles

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -1,9 +1,23 @@
 import appWriteService from "../appWrite/config";
+import { Link } from "react-router-dom";
+import "./PostCard.scss";
 
-const PostCard = () => {
+const PostCard = ({ $id, title, featuredImage }) => {
   return (
-    <div>PostCard</div>
-  )
-}
+    <Link to={`/post/${$id}`} className="post-card-link">
+      <div className="post-card">
+        <div className="post-card__image-container">
+          <img
+            className="post-card__image"
+            src={appWriteService.previewFile(featuredImage)}
+            alt={title}
+          />
+        </div>
 
-export default PostCard
+        <h2 className="post-card__title">{title}</h2>
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
