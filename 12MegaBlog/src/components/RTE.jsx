3c8b58f7c02ca9d167647ef4742da81b8743954a
Shivam <shivamgarade05@gmail.com>
2026-06-09 15:12:33 +0530

    add RTE component

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -1,24 +1,56 @@
 import React from 'react'
 import { Editor } from "@tinymce/tinymce-react"
+import { Controller } from "react-hook-form"
 
-export default function RTE() {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <Editor
-      initialValue='default value'
-      init={{
-        branding: false,
-        height: 500,
-        menubar: true,
-        plugings: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount'
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-      alignleft aligncenter alignright alignjustify | \
-      bullist numlist outdent indent | removeformat | help'
-      }}
-    />
+    <div>
+      {label && <label className="">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: false,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor"
+              ],
+              toolbar:
+                "undo red fore undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+              content_style: "body { font-family: 'Helvetica', sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
   )
 }
+
+// NOTES : control react hook form se aata hai ,and ye hi responsible hai is component ki saari state ko us form mein le jane ke liye
+// control jo bhi parent dega vo controller mein pass kar denge
+// render={({field:{onChange}}) => {}} is fiend mein kuchh bhi agar changes hota hai toh mujhe inform kar dena 
