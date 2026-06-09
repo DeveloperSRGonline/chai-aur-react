75432cffa4481dc88c6b335e6170a128945ad096
Shivam <shivamgarade05@gmail.com>
2026-06-09 09:50:37 +0530

    add config

--- 12MegaBlog/vite.config.js
+++ 12MegaBlog/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: false,
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
+    }
+  },
+  build: {
+    sourcemap: false,
+    cssCodeSplit: false,
+  }
 })
